fbe8bf06f33f6b42075c1b0b210c0ce0b52179d0
Afshin Mokhtari <afshinator@gmail.com>
2025-11-10 15:40:08 -0800

    Update use of SafeView

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
 
 export default function App() {
+  const queryClient = new QueryClient()
+  useReactQueryDevTools(queryClient);
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
-    </>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   );
 }
